1a8a2dce54a69935d429e7c22424103ee0ac8c28
kdnewton <kdnewton@gmail.com>
2025-01-13 09:27:12 -0800

    Tweening camera

---
+++ Scenes/npc_controller.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@onready var _character = get_parent()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _input(event : InputEvent):
+	if (event.is_action_pressed("jump")):
+		_character.jump()
+	if (event.is_action_released("jump")):
+		_character.stop_jump()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta : float):
+	_character.run(Input.get_axis("run_left", "run_right"))
